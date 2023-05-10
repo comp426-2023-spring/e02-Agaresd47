83b24f31d48b94096dea68e85af6a652058b5f4c
Agaresd47 <zdong@unc.edu>
2023-05-10 12:20:40 +0900

    rpsls near finish

--- server.js
+++ server.js
@@ -78,9 +78,8 @@ import minimist from 'minimist'
 import express from 'express'
 
 var argv = minimist(process.argv.slice(2));
-const port = argv.port || 5000;
 
-const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
